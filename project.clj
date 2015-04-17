59762d2842bd825ac6f29a446325357db13e7dbb
SamPeng87 <405614104@qq.com>
2015-04-17 15:14:04 +0800

    init repo

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-markdown "0.1.0-SNAPSHOT"
+  :description "translate from markdown to html with string or file"
+  :url "https://github.com/SamPeng87/elj_markdown"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ test/clj_markdown/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-markdown.core-test
+  (:require [clojure.test :refer :all]
+            [clj-markdown.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
